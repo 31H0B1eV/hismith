3ca14cf6e2ec8e6cf5111596b67cde8d5a4a2762
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-29 22:32:59 +0300

    add symfony/console

--- src/app.php
+++ src/app.php
@@ -5,6 +5,7 @@ use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Knp\Provider\ConsoleServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
@@ -12,6 +13,12 @@ $app->register(new AssetServiceProvider());
 $app->register(new Silex\Provider\VarDumperServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+
+$app->register(new ConsoleServiceProvider(), array(
+    'console.name'              => 'hismith',
+    'console.version'           => '1.0.0',
+    'console.project_directory' => __DIR__.'/console'
+));
 
 $app['twig.path'] = array(__DIR__.'/../views');
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
