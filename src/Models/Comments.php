5dc3df328bd9cb0f5b0ab8367f28cf5030d30cba
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-30 19:02:18 +0300

    fix query result

--- src/Models/Comments.php
+++ src/Models/Comments.php
@@ -18,17 +18,19 @@ class Comments extends Model
                     (SELECT COUNT(l2.id) FROM likes l2 WHERE l2.comment_id = c.id) AS likes                    
                 FROM comments as c";
 
-        return $this->connection->query($sql);
+        return $this->connection->query($sql)->fetchAll();
     }
 
     public function getComment($id)
     {
+        $id = (int) $id;
+
         $sql = "SELECT c.author_name AS author, c.feedback_text AS feedback, c.published_at,
                     INET_NTOA(c.author_ip) AS author_ip,
                     (SELECT COUNT(l2.id) FROM likes l2 WHERE l2.comment_id = c.id) AS likes                    
                 FROM comments as c WHERE c.id = {$id}";
 
-        return $this->connection->query($sql);
+        return $this->connection->query($sql)->fetchAll();
     }
 
 }
