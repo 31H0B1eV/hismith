4e8bdec7bf564456c05d7572be61fa7f50fc8539
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-31 19:20:38 +0300

    add production version

--- database/database.php
+++ database/database.php
@@ -3,19 +3,19 @@
 /**
  * By default used config for local docker environment
  */
-// $DATABASE_HOST = getenv(APP_DATABASE_HOST, true) ?: 'mysql';
-// $DATABASE_NAME = getenv(APP_DATABASE_NAME, true) ?: 'research';
-// $DATABASE_USER = getenv(APP_DATABASE_USER, true) ?: 'homestead';
-// $DATABASE_PASSWORD = getenv(APP_DATABASE_PASSWORD, true) ?: 'secret';
+$DATABASE_HOST = getenv(APP_DATABASE_HOST, true) ?: 'mysql';
+$DATABASE_NAME = getenv(APP_DATABASE_NAME, true) ?: 'research';
+$DATABASE_USER = getenv(APP_DATABASE_USER, true) ?: 'homestead';
+$DATABASE_PASSWORD = getenv(APP_DATABASE_PASSWORD, true) ?: 'secret';
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'dbs.options' => array (
         'mysql' => array(
             'driver'    => 'pdo_mysql',
-            'host'      => 'us-cdbr-iron-east-03.cleardb.net',
-            'dbname'    => 'heroku_47b24447e49f376',
-            'user'      => 'b0e35a2d4ecca0',
-            'password'  => 'aea93765',
+            'host'      => $DATABASE_HOST,
+            'dbname'    => $DATABASE_NAME,
+            'user'      => $DATABASE_USER,
+            'password'  => $DATABASE_PASSWORD,
         ),
     ),
 ));
