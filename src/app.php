50cf359dd9f299e90d4e6740da08010712d9072b
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-31 18:00:04 +0300

    include debug

--- src/app.php
+++ src/app.php
@@ -44,10 +44,10 @@ $app->register(new FormServiceProvider());
 /** Remove it in production */
 
 $app['debug'] = true;
-// $app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
-//     'profiler.cache_dir' => __DIR__.'/../cache/profiler',
-// ));
-// $app->register(new Sorien\Provider\DoctrineProfilerServiceProvider());
+$app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../cache/profiler',
+));
+$app->register(new Sorien\Provider\DoctrineProfilerServiceProvider());
 
 /** =========  */
 
