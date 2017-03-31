6a3959869cbeaa86e1f0398c18b35a6b3459301d
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-31 17:32:15 +0300

    add config for database from env

--- database/database.php
+++ database/database.php
@@ -1,13 +1,21 @@
 <?php
 
+/**
+ * By default used config for local docker environment
+ */
+$DATABASE_HOST = getenv(APP_DATABASE_HOST, true) ?: 'mysql';
+$DATABASE_NAME = getenv(APP_DATABASE_NAME, true) ?: 'research';
+$DATABASE_USER = getenv(APP_DATABASE_USER, true) ?: 'homestead';
+$DATABASE_PASSWORD = getenv(APP_DATABASE_PASSWORD, true) ?: 'secret';
+
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'dbs.options' => array (
         'mysql' => array(
             'driver'    => 'pdo_mysql',
-            'host'      => 'mysql',
-            'dbname'    => 'research',
-            'user'      => 'homestead',
-            'password'  => 'secret',
+            'host'      => $DATABASE_HOST,
+            'dbname'    => $DATABASE_NAME,
+            'user'      => $DATABASE_USER,
+            'password'  => $DATABASE_PASSWORD,
         ),
     ),
 ));
